690f7c2d7dd845b76b8874292a79da5464f9fa92
lburgazzoli <lburgazzoli@gmail.com>
2019-08-19 19:32:29 +0200

    fix typo

--- src/main/java/io/github/lburgazzoli/examples/ApplicationConfigSourceProvider.java
+++ src/main/java/io/github/lburgazzoli/examples/ApplicationConfigSourceProvider.java
@@ -4,10 +4,12 @@ import java.util.Collections;
 
 import org.eclipse.microprofile.config.spi.ConfigSource;
 import org.eclipse.microprofile.config.spi.ConfigSourceProvider;
+import org.slf4j.LoggerFactory;
 
-public class ApplicatiobConfigSourceProvider implements ConfigSourceProvider {
+public class ApplicationConfigSourceProvider implements ConfigSourceProvider {
     @Override
     public Iterable<ConfigSource> getConfigSources(ClassLoader forClassLoader) {
+        LoggerFactory.getLogger(getClass()).info("ApplicationConfigSourceProvider::getConfigSources");
         return Collections.emptyList();
     }
 }
